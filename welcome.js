e5d4f5e852f4018374ec3a840623ad728c2469c4
sri.naresh <sri.naresh1@gmail.com>
2023-08-18 17:18:00 +0530

    hai this is java file practise

---
+++ welcome.js
@@ -0,0 +1,8 @@
+console.log("hai this is first output");
+
+let x=10;
+var y=20.12;
+const z="naresh";
+console.log('x is : '+x+'is the data type of '+typeof x);
+console.log('y is : '+y+' is the data type of '+ typeof y);
+console.log('y is : '+z+' is the data type of '+ typeof z);
